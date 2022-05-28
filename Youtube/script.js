f6b99b6a55365f94bda7f56b96e93a77df62dfe8
limchaekyung <corud505@naver.com>
2022-05-28 23:31:54 +0900

    duration

--- Youtube/script.js
+++ Youtube/script.js
@@ -7,6 +7,9 @@ const miniPlayerBtn = document.querySelector(".mini-player-btn")
 const muteBtn = document.querySelector(".mute-btn")
 const volumeSlider = document.querySelector(".volume-slider")
 
+const currentTimeElem = document.querySelector(".current-time")
+const totalTimeElem = document.querySelector(".total-time")
+
 const videoContainer = document.querySelector(".video-container")
 const video = document.querySelector("video")
 
@@ -33,8 +36,45 @@ document.addEventListener("keydown", e => {
         case "m":
             toggleMute()
             break
+        case "arrowleft":
+        case "j":
+            skip(-5)
+            break
+        case "arrowright":
+        case "l":
+            skip(5)
+            break
+
     }
 })
+
+// Duration
+video.addEventListener("loadeddata", () => {
+    totalTimeElem.textContent = formatDuration(video.duration)
+})
+
+video.addEventListener("timeupdate", () => {
+    currentTimeElem.textContent = formatDuration(video.currentTime)
+})
+
+const leadngZeroFormatter = new Intl.NumberFormat(undefined, {
+    minimumIntegerDigits: 2,
+})
+
+function formatDuration(time) {
+    const seconds = Math.floor(time % 60)
+    const minutes = Math.floor(time / 60) % 60
+    const hours = Math.floor(time / 3600)
+    if (hours === 0) {
+        return `${minutes}:${leadngZeroFormatter.format(seconds)}`
+    } else {
+        return `${hours}:${leadngZeroFormatter.format(minutes)}:${leadngZeroFormatter.format(seconds)}`
+    }
+}
+
+function skip(duration) {
+    video.currentTime += duration
+}
 
 // Volume
 muteBtn.addEventListener("click", toggleMute)

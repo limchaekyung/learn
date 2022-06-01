20cd6bcadd5832e7b2c612b9e7783a7fd036109f
limchaekyung <corud505@naver.com>
2022-06-01 23:57:48 +0900

    기본 연산자

---
+++ Javascript Basic/5. basicOperator.js
@@ -0,0 +1,39 @@
+// +(더하기) -(빼기) *(곱하기) /(나누기) %(나머지)
+
+/* 
+나머지를 어디에 쓸까?
+
+홀수 : X % 2 = 1
+짝수 : Y % 2 = 0
+
+어떤 값이 들어와도 5를 넘기면 안돼
+X % 5 = 0 ~ 4
+*/
+
+/* 
+거듭제곱
+
+const num = 2 ** 3;
+console.log(num);   // 8
+*/
+
+/* 
+우선순위
+* / > + -
+*/
+
+// 연산자 줄여서 쓰기 
+/* let num = 10;
+// num = num + 5;
+num += 5;
+num /= 5;
+num %= 5;
+
+console.log(num) */
+
+// 증가 연산자, 감소 연산자
+let num = 10;
+// let result = num++;  //num의 값을 result에 넣은 후 1 증가
+let result = ++num; //num을 1 증가시킨 값을 result에 넣음
+
+console.log(result)

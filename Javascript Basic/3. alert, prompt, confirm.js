eb0cd25f5e06fa2c7bcfb1c117d0874d45309884
limchaekyung <corud505@naver.com>
2022-06-01 23:30:35 +0900

    alert, prompt, confirm

---
+++ Javascript Basic/3. alert, prompt, confirm.js
@@ -0,0 +1,25 @@
+// alert 알려줌
+// prompt 입력방듬
+// confirm 확인받음
+
+/* const name = prompt("이름을 입력하세요.")
+console.log(name)   // null
+// alert("환영합니다. " + name + "님")
+alert(`안녕하세요, ${name}님. 환영합니다.`) */
+
+/* const reservationDate = prompt("예약일을 입력해주세요.", "2020-10-")
+// 메시지, 디폴트값 순서 */
+
+/* 
+const isAdult = confirm("당신은 성인입니까?")
+console.log(isAdult) // 확인: true, 취소: false
+
+alert('삭제되었습니다.')
+// prompt('예약일을 입력하세요.', '2020-10-')
+// confirm('구독을 취소하시겠습니까?') */
+
+/* 
+단점
+1. 스크립트 일시정지 
+2. 스타일링X
+*/

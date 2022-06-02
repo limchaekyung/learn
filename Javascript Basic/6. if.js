d0ddb5266502b1d2dcae080aa4fe5a358d73ab75
limchaekyung <corud505@naver.com>
2022-06-02 16:22:49 +0900

    조건문

---
+++ Javascript Basic/6. if.js
@@ -0,0 +1,28 @@
+// 비교 연산자
+// < > <= >= == !=
+
+/* console.log(10 > 5)     //true
+console.log(10 == 5)    // 동등연산자, false
+console.log(10 != 5)    // true */
+
+// 동등 연산자
+/* const a = 1;
+const b = "1"
+console.log(a == b) // true
+console.log(a === b)    // false, 일치 연산자 - type까지 비교 */
+
+// if, else, else if
+// 추가요구사항 : 19살이면 수능 잘치세요라는 문구를 보여주세요
+// age === 19
+
+/* const age = 30;
+
+if (age > 19) {
+    console.log('환영합니다.')
+} else if (age === 19) {
+    console.log('수능 잘치세요.')
+} else {
+    console.log('안녕히 가세요')
+}
+
+console.log('-------------------------') */

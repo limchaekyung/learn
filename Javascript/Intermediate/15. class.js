a7835ba1cc3567263f0f70bb4eff757ebcbddac8
limchaekyung <corud505@naver.com>
2022-06-16 23:43:52 +0900

    class

--- Javascript/Intermediate/15. class.js
+++ Javascript/Intermediate/15. class.js
@@ -1,11 +1,14 @@
 // class
 // ES6에 추가된 스펙
-const User = function (name, age) {
+/* const User = function (name, age) {
     this.name = name
     this.age = age
-    this.showName = function () {
-        console.log(this.name)
-    }
+    // this.showName = function () {
+    //     console.log(this.name)
+    // }
+}
+User.prototype.showName = function () {
+    console.log(this.name)
 }
 const Mike = new User("Mike", 40)
 
@@ -18,4 +21,75 @@ class User2 {
         console.log(this.name)
     }
 }
-const tom = new User2("Tom", 19)
+const tom = new User2("Tom", 19) */
+
+// extends(상속)
+/* class Car {
+    constructor(color) {
+        this.color = color
+        this.wheels = 4
+    }
+    drive() {
+        console.log("drive..")
+    }
+    stop() {
+        console.log("STOP!")
+    }
+}
+
+class Bmw extends Car {
+    park() {
+        console.log("PARK")
+    }
+}
+const z4 = new Bmw('blue') */
+
+// 메소드 오버라이딩(method overriding)
+/* class Car {
+    constructor(color) {
+        this.color = color
+        this.wheels = 4
+    }
+    drive() {
+        console.log("drive..")
+    }
+    stop() {
+        console.log("STOP!")
+    }
+}
+
+class Bmw extends Car {
+    park() {
+        console.log("PARK")
+    }
+    stop() {
+        super.stop()
+        console.log("OFF")
+    }
+}
+const z4 = new Bmw('blue') */
+
+// 오버라이딩(overriding)
+class Car {
+    constructor(color) {    //{}
+        this.color = color
+        this.wheels = 4
+    }
+    drive() {
+        console.log("drive..")
+    }
+    stop() {
+        console.log("STOP!")
+    }
+}
+
+class Bmw extends Car {
+    constructor(color) {
+        super(color)
+        this.navigation = 1
+    }
+    park() {
+        console.log("PARK")
+    }
+}
+// const z4 = new Bmw('blue')
